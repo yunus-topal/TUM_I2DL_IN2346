709611c5ad822a6adc4d53570b51be19b191f082
Yunus Emre Topal <emretopal20@gmail.com>
2022-12-05 20:01:07 +0100

    exercise 6

---
+++ Exercises/exercise_06/exercise_code/data/__init__.py
@@ -0,0 +1,13 @@
+"""Definition of all datasets and dataloader"""
+
+from .base_dataset import DummyDataset
+from .image_folder_dataset import (
+    ImageFolderDataset,
+    MemoryImageFolderDataset,
+    RescaleTransform,
+    NormalizeTransform,
+    FlattenTransform,
+    ComposeTransform,
+    compute_image_mean_and_std,
+)
+from .dataloader import DataLoader
